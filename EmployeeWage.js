f378f19b5436424a5210912c966700b6280767f9
usha-12 <usha12ahirwar@gmail.com>
2022-11-29 13:12:21 +0530

    Calculate Total Wage

---
+++ EmployeeWage.js
@@ -0,0 +1,26 @@
+console.log("----------Welcome to EmpWage---------");
+const  FULL_TIME_HRS= 8;
+const  PART_TIME_HRS= 4;
+const  PARTTIME= 1;
+const  FULLTIME= 2;
+const  WAGE_PER_HRS= 20;
+
+let empHrs=0;
+var empCheck=Math.floor(Math.random()*10)%3;
+
+switch(empCheck){
+    case PARTTIME:
+        console.log("Employee is Parttime");
+        empHrs = PART_TIME_HRS;
+        break;
+        case FULLTIME:
+            console.log("Employee is Fulltime");
+            empHrs = FULLTIME;
+            break;
+            default:
+                console.log("Employee is Abscent");
+                empHrs = 0;
+}
+let empWage = empHrs*WAGE_PER_HRS;
+
+console.log("EmpWage :"+empWage)
