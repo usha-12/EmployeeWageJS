469e5652e6d21288fb109a4b9ad53f4c6c10e4ba
usha-12 <usha12ahirwar@gmail.com>
2022-11-29 13:19:16 +0530

    Refactor Daily Wage

---
+++ EmployeeWage.js
@@ -0,0 +1,27 @@
+console.log("----------Welcome to EmpWage---------");
+const  FULL_TIME_HRS= 8;
+const  PART_TIME_HRS= 4;
+const  PARTTIME= 1;
+const  FULLTIME= 2;
+const  WAGE_PER_HRS= 20;
+
+function getWoirkingHrs(empCheck){
+switch(empCheck){
+    case PARTTIME:
+        console.log("Employee is Parttime");
+        return PART_TIME_HRS;
+        break;
+    case FULLTIME:
+        console.log("Employee");
+        return FULL_TIME_HRS;
+        break;
+    default:
+        console.log("EmpWage :"+empWage)
+        return 0;
+}
+}
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10) % 3;
+empHrs = getWoirkingHrs(empCheck);
+let empWage = empHrs*WAGE_PER_HRS;
+console.log("EmpWage : "+empWage)
